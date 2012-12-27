fa1d178e51318591e9a419ceef25b3c4c9530ab5
Hrishikesh Huilgolkar <hrishikesh911@gmail.com>
2012-12-27 23:04:35 +0530

    second result of LKH is same

--- tools/computedist.py
+++ tools/computedist.py
@@ -1,14 +1,46 @@
-def calc(file,type):
+import math
+import csv
+def extractpath(file,type):
 	with open(file,"rb") as f:
 		if type=="tsp":
 			path=[int (i) for i in f.read().splitlines()[6:-2]]
 		if type=="csv":
 			path1=[i for i in f.read().splitlines()[1:]]
-			path=[int(i.split(",")[0]) for i in path1]
+			path=[int(i.split(",")[0])+1 for i in path1]
 			
 	return path
-if __name__ == "__main__":
-	path=calc("besttour_tot.tsp","tsp")
-	path=calc("raw/random_paths_benchmark.csv","csv")
+def euc(c1,c2):
+	#print c1,c2
+	x1,y1=c1[0],c1[1]
+	x2,y2=c2[0],c2[1]
+	#print x1,x2,y1,y2
+	return math.sqrt((x2-x1)**2+(y2-y1)**2)
+
+def dist(path):
+	coords={}
+	with open("../raw/santa_cities.csv","rb") as f:
+		for i in csv.reader(f):
+			coords[int(i[0])+1]=[int(j) for j in i[1:]]
+	dist=0
 	print len(path)
-	print path[:5]
+	#print path[142722]
+	#print coords[path[142721]]
+	#print coords[path[142722]]
+	#print euc(coords[path[142721]],coords[path[142722]])
+	for i in range(len(path)-1):
+	#for i in range(4):
+		#print i
+		dist+=euc(coords[path[i]],coords[path[i+1]])
+		#print dist
+	return dist
+
+
+if __name__ == "__main__":
+	path=extractpath("../results/besttour_tot.tsp","tsp")
+	print len(path)
+	print dist(path)
+	path2=extractpath("../results/besttour_tot2.tsp","tsp")
+	#path=extractpath("../raw/random_paths_benchmark.csv","csv")
+	print len(path2)
+	#print path[142720:142725]
+	print dist(path2)
--- tools/intersection.py
+++ tools/intersection.py
@@ -1,17 +1,17 @@
-from computedist import calc
+from computedist import extractpath
 def intersect(file1,type1,file2,type2):
-	p1l=calc(file1,type1)
+	p1l=extractpath(file1,type1)
 	p1l2=[-1]+(p1l)
 	p1=zip(p1l,p1l2)
 	p1s=set([frozenset(i)for i in p1])
 
-	p2l=calc(file2,type2)
+	p2l=extractpath(file2,type2)
 	p2l2=[-1]+(p2l)
 	p2=zip(p2l,p2l2)
 	p2s=set([frozenset(i)for i in p2])
 	return p1s.intersection(p2s)
 
 if __name__=="__main__":
-	res=intersect("besttour_tot.tsp","tsp","raw/random_paths_benchmark.csv","csv")
+	res=intersect("../results/besttour_tot.tsp","tsp","../results/besttour_tot2.tsp","tsp")
 	print len(res)
-	print res
+	#print res
