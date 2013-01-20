e4cf722aefc005696ddb125d72ac41928210974f
Hrishikesh Huilgolkar <hrishikesh911@gmail.com>
2013-01-20 15:37:17 +0530

    Added readme

--- tools/computedist.py
+++ tools/computedist.py
@@ -43,7 +43,8 @@ def validate(path):
 
 
 if __name__ == "__main__":
-	path=extractpath("../LKH/results/tiny20_1/fans2.csv","csv")
+	#path=extractpath("../LKH/results/tiny20_1/fans2.csv","csv")
+	path=extractpath("../LKH/results/time300/fans.csv","csv")
 	print len(path)
 	print path[:5]
 	print dist(path)
